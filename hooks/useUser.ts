6ff14a13d65a16248be4209055eea5a8406f188b
devrazzak <developerrazzak@gmail.com>
2025-02-15 00:09:41 +0600

    test

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,13 +1,13 @@
-import { useQuery } from '@tanstack/react-query';
 import api from '@/services/api';
 import type { User } from '@/types/auth';
+import { useQuery } from '@tanstack/react-query';
 
 export function useUser(userId: string) {
-  return useQuery({
-    queryKey: ['user', userId],
-    queryFn: async () => {
-      const { data } = await api.get<User>(`/users/${userId}`);
-      return data;
-    },
-  });
+    return useQuery({
+        queryKey: ['user', userId],
+        queryFn: async () => {
+            const { data } = await api.get<User>(`/users/${userId}`);
+            return data;
+        },
+    });
 }
